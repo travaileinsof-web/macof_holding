c8dbe6df0f404545176d4fd62284093b2cd3dfae
Billy Doumbouya <192708154+Clinton62395@users.noreply.github.com>
2026-09-15 18:01:06 +0000

    fix(ui): update image for MACOF Print in Domaines section

--- api/update_filiale.ts
+++ api/update_filiale.ts
@@ -3,11 +3,11 @@ import { filiales } from "./src/db/schema";
 import { eq } from "drizzle-orm";
 
 async function main() {
-  console.log("Updating MACOF Immobilier SARL image...");
+  console.log("Updating MACOF Print & Com SARL image...");
   
   await db.update(filiales).set({
-    image_url: "https://media.istockphoto.com/id/2151134721/photo/happy-group-of-business-people-standing.jpg?s=612x612&w=0&k=20&c=iTFwKo-IkwoBxrpb-BqwyUR6bK3DrCnnw3pB_wQN-O0="
-  }).where(eq(filiales.slug, "immobilier"));
+    image_url: "https://media.istockphoto.com/id/1492549645/photo/black-friday-shoppers-tired-of-waiting.jpg?s=612x612&w=0&k=20&c=WAUcS_XmMOQ9ZhVR7NwOxm9fXjxgZmuST468rRIQv3s="
+  }).where(eq(filiales.slug, "print"));
 
   console.log("Done.");
   process.exit(0);
